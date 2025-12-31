e804c1bd7a7a869fe395212f5d4e4c099932b3ef
Musa-Kal <131704022+Musa-Kal@users.noreply.github.com>
2025-12-31 14:09:24 -0500

    slm.py wip

---
+++ slm.py
@@ -0,0 +1,38 @@
+# ======== Model Params ========
+training_text_path = r'dataset\tinyshakespeare.txt'
+training_split = 0.85
+context_size = 10
+# ==============================
+
+# training loading text
+text = ""
+with open(training_text_path) as f:
+    text = f.read()
+
+
+# getting all the chars from within the text
+chars = sorted(set(text))
+
+# maps to encode and decode text
+char_i_map = {c:i for i,c in enumerate(chars)}
+i_char_map = {i:c for i,c in enumerate(chars)}
+
+encode = lambda seq: [char_i_map[c] for c in seq]
+decode = lambda seq: "".join(i_char_map[i] for i in seq)
+
+
+import torch
+
+# tokenizing training data
+data = torch.tensor(encode(text), dtype=torch.long)
+print("=== Data Encoded ===")
+print(data.shape, data.dtype)
+
+# train test split
+if training_split > 1 or training_split <= 0:
+    raise ValueError(f"training_split was {training_split}, it must be 0 < training_split <= 1")
+
+num = int(len(data)*training_split)
+training_data = data[:num]
+testing_data = data[num:]
+
