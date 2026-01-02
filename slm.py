7b4a871b076536f6dd7a289e37c4729c21446cd7
Musa-Kal <131704022+Musa-Kal@users.noreply.github.com>
2026-01-01 19:38:25 -0500

    get_batch implemented

--- slm.py
+++ slm.py
@@ -2,7 +2,16 @@
 training_text_path = r'dataset\tinyshakespeare.txt'
 training_split = 0.85
 context_size = 10
+batch_size = 4
+torch_seed = 1337
 # ==============================
+
+# importing required modules
+import torch
+
+if type(torch_seed) == int:
+    torch.manual_seed(torch_seed)
+    print(f"=== torch seed seed to {torch_seed} ===")
 
 # training loading text
 text = ""
@@ -21,8 +30,6 @@ encode = lambda seq: [char_i_map[c] for c in seq]
 decode = lambda seq: "".join(i_char_map[i] for i in seq)
 
 
-import torch
-
 # tokenizing training data
 data = torch.tensor(encode(text), dtype=torch.long)
 print("=== Data Encoded ===")
@@ -36,3 +43,10 @@ num = int(len(data)*training_split)
 training_data = data[:num]
 testing_data = data[num:]
 
+def get_batch(data, batch_size=batch_size):
+    ix = torch.randint(len(data) - context_size, (batch_size,))
+    x = torch.stack([data[i : i+context_size] for i in ix])
+    y = torch.stack([data[i+1 : i+context_size+1] for i in ix])
+    return x, y
+
+print(get_batch(training_data))
